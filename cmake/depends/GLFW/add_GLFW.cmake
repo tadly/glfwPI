d316308b3165c9bc2b9d147a4f0a84067e760218
Tadly <jan@tadly.de>
2016-07-22 13:08:28 +0200

    some more cmake tweaks

--- cmake/depends/GLFW/add_GLFW.cmake
+++ cmake/depends/GLFW/add_GLFW.cmake
@@ -1,19 +1,24 @@
-set(GLFW_TARGET ${CMAKE_BINARY_DIR}/3rdParty)
+set(GLFW_SRC_DIR ${CMAKE_BINARY_DIR}/3rdParty/GLFW-src)
+set(GLFW_BUILD_DIR ${CMAKE_BINARY_DIR}/3rdParty/GLFW-build)
 
 # Download and unpack GLFW at configure time
-configure_file(${CMAKE_CURRENT_LIST_DIR}/GLFW-CMakeLists.txt.in
-               ${GLFW_TARGET}/GLFW-download/CMakeLists.txt)
+configure_file(${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt.in
+               ${GLFW_BUILD_DIR}/CMakeLists.txt)
+
+execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
+                WORKING_DIRECTORY ${GLFW_BUILD_DIR})
+
+execute_process(COMMAND ${CMAKE_COMMAND} --build .
+                WORKING_DIRECTORY ${GLFW_BUILD_DIR})
+
 
 set(GLFW_BUILD_DOCS OFF CACHE BOOL "" FORCE)
 set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
 set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
 
-execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
-  WORKING_DIRECTORY ${GLFW_TARGET}/GLFW-download )
 
-execute_process(COMMAND ${CMAKE_COMMAND} --build .
-  WORKING_DIRECTORY ${GLFW_TARGET}/GLFW-download )
+add_subdirectory(${GLFW_SRC_DIR}
+                 ${GLFW_BUILD_DIR})
 
-# Add GLFW directly to our build.
-add_subdirectory(${GLFW_TARGET}/GLFW-src
-                 ${GLFW_TARGET}/GLFW-build)
+
+mark_as_advanced(FORCE ${GLFW_SRC_DIR} ${GLFW_BUILD_DIR})
--- cmake/depends/PictureIt/add_PictureIt.cmake
+++ cmake/depends/PictureIt/add_PictureIt.cmake
@@ -1,16 +1,19 @@
-set(PICTUREIT_TARGET ${CMAKE_BINARY_DIR}/3rdParty)
+set(PICTUREIT_SRC_DIR ${CMAKE_BINARY_DIR}/3rdParty/PictureIt-src)
+set(PICTUREIT_BUILD_DIR ${CMAKE_BINARY_DIR}/3rdParty/PictureIt-build)
 
 # Download and unpack PictureIt at configure time
-configure_file(${CMAKE_CURRENT_LIST_DIR}/PictureIt-CMakeLists.txt.in
-               ${PICTUREIT_TARGET}/PictureIt-download/CMakeLists.txt)
+configure_file(${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt.in
+               ${PICTUREIT_BUILD_DIR}/CMakeLists.txt)
 
 execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
-  WORKING_DIRECTORY ${PICTUREIT_TARGET}/PictureIt-download )
+                WORKING_DIRECTORY ${PICTUREIT_BUILD_DIR})
 
 execute_process(COMMAND ${CMAKE_COMMAND} --build .
-  WORKING_DIRECTORY ${PICTUREIT_TARGET}/PictureIt-download )
+                WORKING_DIRECTORY ${PICTUREIT_BUILD_DIR})
 
 
-# Add PictureIt directly to our build.
-add_subdirectory(${PICTUREIT_TARGET}/PictureIt-src
-                 ${PICTUREIT_TARGET}/PictureIt-build)
+add_subdirectory(${PICTUREIT_SRC_DIR}
+                 ${PICTUREIT_BUILD_DIR})
+
+
+mark_as_advanced(FORCE ${PICTUREIT_SRC_DIR} ${PICTUREIT_BUILD_DIR})
