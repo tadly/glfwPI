56d41a255d84977c224ca6edee25a192f68ebe14
AchimTuran <mail@achim-turan.de>
2016-07-25 10:46:12 +0200

    [cmake] Clean up of importURLProject

--- cmake/cmake_helpers/importURLProject.cmake
+++ cmake/cmake_helpers/importURLProject.cmake
@@ -50,21 +50,12 @@ function(importURLProject)
     endif()
     set(${DL_ARGS_PROJECT}_BINARY_DIR "${DL_ARGS_BINARY_DIR}" PARENT_SCOPE)
     set(${DL_ARGS_PROJECT}_SOURCE_DIR "${DL_ARGS_SOURCE_DIR}" PARENT_SCOPE)
-    set(${DL_ARGS_PROJECT}_BINARY_DIR "${DL_ARGS_BINARY_DIR}" PARENT_SCOPE)
 
     # Create and build a separate CMake project to carry out the download.
     # If we've already previously done these steps, they will not cause
     # anything to be updated, so extra rebuilds of the project won't occur.
     configure_file("${_cmake_helpers_dir}/importURLProject.CMakeLists.txt.in"
                    "${DL_ARGS_BINARY_DIR}/CMakeLists.txt")
-    execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
-                    ${OUTPUT_QUIET}
-                    WORKING_DIRECTORY "${DL_ARGS_BINARY_DIR}"
-    )
-    execute_process(COMMAND ${CMAKE_COMMAND} --build .
-                    ${OUTPUT_QUIET}
-                    WORKING_DIRECTORY "${DL_ARGS_BINARY_DIR}"
-    )
-
+    execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" . ${OUTPUT_QUIET} WORKING_DIRECTORY "${DL_ARGS_BINARY_DIR}")
+    execute_process(COMMAND ${CMAKE_COMMAND} --build . ${OUTPUT_QUIET} WORKING_DIRECTORY "${DL_ARGS_BINARY_DIR}")
 endfunction()
-
