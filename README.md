1dc9b5a8861794020d4ff1f40778393e33842f2a
tadly <git@tadly.de>
2017-08-24 10:38:39 +0200

    reflect PictureIt refactor changes

--- README.md
+++ README.md
@@ -1,27 +1,13 @@
 glfwPI - PictureIt test project
 ================================
-[![Build Status](https://travis-ci.org/Tadly/glfwPI.svg?branch=master)](https://travis-ci.org/Tadly/glfwPI)
+[![Build Status](https://travis-ci.org/Tadly/glfwPI.svg?branch=pi_refactor)](https://travis-ci.org/Tadly/glfwPI)
 
 Just a test project I use to work on [PictureIt](https://github.com/linuxwhatelse/PictureIt)
 
+
 ## Requirements
-If you're on a typical linux installation (with xorg and pulseaudio)  
-you should be just fine.
+If you're on a typical linux installation (with xorg and pulseaudio) you should be just fine.
 
-If you'r trying to build this project on a minimal linux system,  
-you might have to install some libs first though.
-
-The following **Ubuntu** package names may vary depending on your distribution:
-* libsndfile1-dev
-* libpulse-dev
-* libgl1-mesa-dev
-* libglu1-mesa-dev
-* libx11-dev
-* libxcursor-dev
-* libxi-dev
-* libxinerama-dev
-* libxrandr-dev
-* libxxf86vm-dev
 
 ## Build instructions
 Here, have some Copy&Paste help :)
@@ -30,19 +16,12 @@ git clone https://github.com/Tadly/glfwPI
 cd glfwPI
 
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=./install ../
+cmake -DCMAKE_INSTALL_PREFIX=. ../
 
 make install
 ```
-After that, you should have a `bin/glfwPI` executable
 
 ## Usage
-Super easy (the command-line would tell you the same)
 ```bash
-bin/glfwPI img-directory [-b spectrum-bar-count] [-a audio-file]
+bin/glfwPI [img-directory]
 ```
-[Check here](http://www.mega-nerd.com/libsndfile/) for a list of supported audio formats
-(I tested \*.wav only though)
-
-On startup *glfwPI* will print some keyboard-shortcuts you
-can use to mess around with it :)
--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,24 @@
 #include <math.h>
 #include <signal.h>
 #include <thread>
+#include <atomic>
 
-#include <GLFW/glfw3.h>
+#define GL_GLEXT_PROTOTYPES
+#include <GL/gl.h>
+
 #include <PictureIt/pictureit.hpp>
 
-#include "audio_player.hpp"
+#include <GLFW/glfw3.h>
+
+#include "pulse.hpp"
 
 
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 720
 
-#define IMG_UPDATE_INTERVAL 30
-
-
-int SPECTRUM_BAR_COUNT = 64;
-int TRANSITION_TIME_MS = 500;
-int slide_direction = 1;
-
 const char *image_dir_path = nullptr;
-const char *audio_file_path = nullptr;
 
-GLFWwindow *window = nullptr;
 PictureIt *pi = nullptr;
-AudioPlayer *player = nullptr;
 
 int current_windowed_width = WINDOW_WIDTH;
 int current_windowed_height = WINDOW_HEIGHT;
@@ -35,11 +30,37 @@ int current_windowed_pos_y = 0;
 bool fullscreen = false;
 bool quit = false;
 
+const std::chrono::time_point<std::chrono::high_resolution_clock> t_start =
+    std::chrono::high_resolution_clock::now();
+
+
+class Input_thread {
+private:
+    std::atomic<bool> running;
+    std::thread th_input;
+
+public:
+    template <typename Buf> Input_thread(Input &i, Buf &buffer):
+        running(true), th_input([&] {
+            while(running) {
+                i.read(buffer);
+            };
+        }){};
+
+    ~Input_thread(){
+        running = false;
+        th_input.join();
+    };
+};
+
+
+
 static void signal_handler(int sig, siginfo_t *si, void *unused) {
     quit = true;
 }
 
-static void keyboard(GLFWwindow* window, int key, int scancode, int action, int mods) {
+static void keyboard(GLFWwindow* window, int key, int scancode, int action,
+                    int mods) {
     if (action != GLFW_PRESS && action != GLFW_REPEAT)
         return;
 
@@ -49,13 +70,20 @@ static void keyboard(GLFWwindow* window, int key, int scancode, int action, int 
 
             if (fullscreen) {
                 // Remember position and size
-                const GLFWvidmode *mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-                glfwGetWindowPos(window, &current_windowed_pos_x, &current_windowed_pos_y);
-                glfwGetWindowSize(window, &current_windowed_width, &current_windowed_height);
+                const GLFWvidmode *mode = glfwGetVideoMode(
+                    glfwGetPrimaryMonitor());
+                glfwGetWindowPos(window, &current_windowed_pos_x,
+                                 &current_windowed_pos_y);
+                glfwGetWindowSize(window, &current_windowed_width,
+                                  &current_windowed_height);
 
-                glfwSetWindowMonitor(window, glfwGetPrimaryMonitor(), 0, 0, mode->width, mode->height, 0);
+                glfwSetWindowMonitor(window, glfwGetPrimaryMonitor(), 0, 0,
+                                     mode->width, mode->height, 0);
             } else {
-                glfwSetWindowMonitor(window, NULL, current_windowed_pos_x, current_windowed_pos_y, current_windowed_width, current_windowed_height, 0);
+                glfwSetWindowMonitor(window, NULL, current_windowed_pos_x,
+                                     current_windowed_pos_y,
+                                     current_windowed_width,
+                                     current_windowed_height, 0);
             }
             break;
 
@@ -64,113 +92,17 @@ static void keyboard(GLFWwindow* window, int key, int scancode, int action, int 
             break;
 
         case GLFW_KEY_N:
-            pi->update_image();
+            pi->next_image();
             break;
 
         case GLFW_KEY_S:
-            pi->spectrum_enabled = !pi->spectrum_enabled;
-            break;
-
-        case GLFW_KEY_V:
-            if (mods == GLFW_MOD_CONTROL) {
-                pi->spectrum_flip_vertical = !pi->spectrum_flip_vertical;
-            } else {
-                pi->spectrum_mirror_vertical = !pi->spectrum_mirror_vertical;
-            }
-            break;
-
-        case GLFW_KEY_H:
-            if (mods == GLFW_MOD_CONTROL) {
-                pi->spectrum_flip_horizontal = !pi->spectrum_flip_horizontal;
-            } else {
-                pi->spectrum_mirror_horizontal = !pi->spectrum_mirror_horizontal;
-            }
-            break;
-
-        case GLFW_KEY_R:
-            pi->img_pick_random = !pi->img_pick_random;
-            break;
-
-        case GLFW_KEY_B:
-            pi->spectrum_position_vertical = 1.0f;
-            break;
-
-        case GLFW_KEY_C:
-            pi->spectrum_position_vertical = 0.0f;
-            break;
-
-        case GLFW_KEY_T:
-            pi->spectrum_position_vertical = -1.0f;
-            break;
-
-        case GLFW_KEY_UP:
-            pi->spectrum_position_vertical = pi->spectrum_position_vertical - 0.01f;
-            break;
-
-        case GLFW_KEY_DOWN:
-            pi->spectrum_position_vertical = pi->spectrum_position_vertical + 0.01f;
-            break;
-
-        case GLFW_KEY_LEFT:
-            pi->spectrum_position_horizontal = pi->spectrum_position_horizontal - 0.01f;
-            break;
-
-        case GLFW_KEY_RIGHT:
-            pi->spectrum_position_horizontal = pi->spectrum_position_horizontal + 0.01f;
-            break;
-
-        case GLFW_KEY_RIGHT_BRACKET:
-            pi->spectrum_width = pi->spectrum_width + 0.015f;
-            break;
-
-        case GLFW_KEY_SLASH:
-            pi->spectrum_width = pi->spectrum_width - 0.015f;
-            break;
-
-        case GLFW_KEY_1:
-            pi->set_img_transition_efx( EFX::CROSSFADE );
-            pi->efx->configure("fade_time_ms",  &TRANSITION_TIME_MS);
-            break;
-
-        case GLFW_KEY_2:
-            pi->set_img_transition_efx( EFX::SLIDE );
-            pi->efx->configure("slide_time_ms", &TRANSITION_TIME_MS);
-            pi->efx->configure("slide_direction", &slide_direction);
-            break;
-
-        case GLFW_KEY_3:
-            //pi->set_img_transition_efx( EFX::FLIP );
-            break;
-
-        case GLFW_KEY_7:
-            pi->efx->image_mode = MODE::STRETCH;
-            break;
-
-        case GLFW_KEY_8:
-            pi->efx->image_mode = MODE::CENTER;
-            break;
-
-        case GLFW_KEY_9:
-            pi->efx->image_mode = MODE::SCALE;
-            break;
-
-        case GLFW_KEY_0:
-            pi->efx->image_mode = MODE::ZOOM;
             break;
     }
 }
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    glViewport(0, 0, width, height);
-
-    pi->window_width = width;
-    pi->window_height = height;
-}
-
-
-void audio_data(float *data, int data_length) {
-    pi->audio_data(data, data_length);
+    pi->set_window_size(width, height);
 }
 
 
@@ -193,34 +125,6 @@ bool add_signal_handler() {
     return true;
 }
 
-void show_help() {
-    std::cout << "Available keyboard shortcuts:" << std::endl;
-    std::cout << "    esc: Exit PictureIt" << std::endl;
-    std::cout << "    f11: Toggle fullscreen" << std::endl;
-    std::cout << "      n: Display next image" << std::endl;
-    std::cout << "      r: Toggle pick random image" << std::endl;
-    std::cout << "      1: Activate Crossfade transition effect" << std::endl;
-    std::cout << "      2: Activate Slide transition effect" << std::endl;
-    std::cout << "      s: Enable/Disable Spectrum" << std::endl;
-    std::cout << "      v: Toggle Spectrum vertical mirror" << std::endl;
-    std::cout << "      h: Toggle Spectrum horizontal mirror" << std::endl;
-    std::cout << " ctrl+v: Toggle Spectrum flip vertical" << std::endl;
-    std::cout << " ctrl+h: Toggle Spectrum flip horizontal" << std::endl;
-    std::cout << "     up: Move Spectrum up" << std::endl;
-    std::cout << "   down: Move Spectrum down" << std::endl;
-    std::cout << "   left: Move Spectrum left" << std::endl;
-    std::cout << "  right: Move Spectrum right" << std::endl;
-    std::cout << "      +: Increase Spectrum size" << std::endl;
-    std::cout << "      -: Decrease Spectrum size" << std::endl;
-    std::cout << "      b: Position Spectrum at the bottom" << std::endl;
-    std::cout << "      c: Position Spectrum at the center" << std::endl;
-    std::cout << "      t: Position Spectrum at the top" << std::endl;
-    std::cout << "      7: Image mode \"Stretch\"" << std::endl;
-    std::cout << "      8: Image mode \"Center\"" << std::endl;
-    std::cout << "      9: Image mode \"Scale\"" << std::endl;
-    std::cout << "      0: Image mode \"Zoom\"" << std::endl;
-}
-
 
 int main(int argc, char **argv) {
     // Signal handling
@@ -230,28 +134,14 @@ int main(int argc, char **argv) {
 
     // Print CLI usage if not enough sys args
     if (argc < 2) {
-        printf("usage: %s img-directory [-b spectrum-bar-count] [-a audio-file]\n", argv[0]);
+        printf("usage: %s [img-directory]\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    // Some simple argparsing
-    // Not super failsafe but'll do :)
+    // Get a image directory
     for (int i = 1; i < argc; i++) {
-        if(strncmp(argv[i], "-b", 2) == 0) {
-            SPECTRUM_BAR_COUNT = atoi(argv[i+1]);
-            i++;
-
-        } else if(strncmp(argv[i], "-a", 2) == 0) {
-            audio_file_path = argv[i+1];
-            i++;
-
-        } else {
-            image_dir_path = argv[i];
-        }
+        image_dir_path = argv[i];
     }
-
-    // Print keyboard shortcuts
-    show_help();
 
 
     // Initialize glfw
@@ -260,67 +150,86 @@ int main(int argc, char **argv) {
     }
 
     // Create a windowed mode window and its OpenGL context
-    window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "glfwPI", NULL, NULL);
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT,
+                                          "glfwPI", NULL, NULL);
 
     if (!window) {
+        fprintf(stderr, "Couldn't create window, exiting!\n");
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
 
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(2);
-
-    glfwSetKeyCallback(window, keyboard);
-
-
-    // Create PictureIt and set some intial properties
-    pi = new PictureIt(SPECTRUM_BAR_COUNT);
-
-    pi->set_img_transition_efx(EFX::CROSSFADE);
-    pi->efx->configure("fade_time_ms",  &TRANSITION_TIME_MS);
-
-    pi->efx->image_mode = MODE::SCALE;
-
-    pi->window_width = WINDOW_WIDTH;
-    pi->window_height = WINDOW_HEIGHT;
-    pi->img_pick_random = true;
-    pi->img_update_interval = IMG_UPDATE_INTERVAL;
-    pi->spectrum_animation_speed = 0.008f;
-    pi->spectrum_position_vertical = 0.0f;
-    pi->spectrum_enabled = true;
-
-    // Get some sexy pink going :)
-    // That's "E91E63" in hex for anyone wondering ;)
-    for (int i = 0; i < SPECTRUM_BAR_COUNT; i++) {
-        pi->set_bar_color(i, 0.914, 0.118, 0.388);
-    }
-
-    // Load images from the directory passed as argument
-    pi->load_images(image_dir_path);
-
-
     // Make the window's context current
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetKeyCallback(window, keyboard);
+
+    fprintf(stdout, "GL: %s, GLSL: %s\n", glGetString(GL_VERSION),
+            glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+    // Create PictureIt and set some initial properties
+    PI::Config::Transition pi_trans;
+    pi_trans.speed = 50;
+
+    PI::Config::PictureIt pi_cfg = PI::Config::PictureIt();
+    pi_cfg.max_fps = 120;
+    pi_cfg.update_interval = 15;
+    pi_cfg.pick_random = true;
+    pi_cfg.recursive = true;
+    pi_cfg.display_mode = PI::Config::Texture::Mode::STRETCH;
+    pi_cfg.transition = pi_trans;
+    pi_cfg.image_dir = image_dir_path;
+
+    pi = new PictureIt(pi_cfg);
+    pi->set_window_size(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    PI::Config::FFT fft_cfg = PI::Config::FFT();
+    fft_cfg.window = PI::Config::FFT::Window::EXACT_BLACKMAN;
+
+    // Spectrum one on the left
+    PI::Config::Spectrum spectrum_cfg1 = PI::Config::Spectrum(fft_cfg);
+    spectrum_cfg1.gradient = -2;
+    spectrum_cfg1.bot_color[0] = 1;
+    spectrum_cfg1.bot_color[1] = 1;
+    spectrum_cfg1.bot_color[2] = 1;
+    spectrum_cfg1.pos.Ymax = 3.5;
+    spectrum_cfg1.pos.Xmin = -1.0;
+    spectrum_cfg1.pos.Xmax = 3.0005;
+    spectrum_cfg1.set_gravity(2.5);
+    spectrum_cfg1.set_db_range(-45, 45);
+    spectrum_cfg1.set_freq_range(0, 1000);
+    pi->add_spectrum(spectrum_cfg1);
+
+    // Mirrored spectrum one on the right
+    spectrum_cfg1.pos.Ymax = 3.5;
+    spectrum_cfg1.pos.Xmin = 3.0005;
+    spectrum_cfg1.pos.Xmax = -1.0;
+    pi->add_spectrum(spectrum_cfg1);
 
 
-    // Create an AudioPlayer so we have something
-    // to feed the spectrum with
-    player = new AudioPlayer(audio_data);
-    if (audio_file_path != nullptr && player->load(audio_file_path)) {
-        player->play();
-    }
+    FFT fft(fft_cfg);
+    Buffer<int16_t> buffer(fft_cfg.get_buffer_size());
+
+    // start input thread
+    Input::Ptr input = make_unique<Pulse>("", fft_cfg.get_sample(), 220,
+                                          false);
+    Input_thread inth(*input, buffer);
 
 
     // Mainloop which renders PictureIt
-    while (!glfwWindowShouldClose(window)) {
-        if (quit) {
-            break;
-        }
+    while (! glfwWindowShouldClose(window)) {
+        if (quit) break;
+
+        fft.calculate(buffer);
 
         // Render PictureIt frame
-        pi->render();
+        pi->draw(fft);
 
         // Swap front and back buffers
         glfwSwapBuffers(window);
@@ -329,10 +238,6 @@ int main(int argc, char **argv) {
         glfwPollEvents();
     }
 
-    // Cleanup
-    player->exit();
-
-    delete player;
     delete pi;
 
     glfwDestroyWindow(window);
